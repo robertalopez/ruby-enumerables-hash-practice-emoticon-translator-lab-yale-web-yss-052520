95fbf31d5b862ede827a0e477675dde419f84732
Robert Lopez <robert.lopez@yale.edu>
2020-05-15 04:34:54 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,24 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
+def load_library(filepath)
   # code goes here
+ emojis = YAML.load_file(filepath)  
+  library = {}
+  emojis.each do |key, value| 
+      
+    if !library[key]
+      library[key] = {}
+    end 
+    if !library[key][:english]
+    library[key][:english] = value[0]
+    end
+    if !library[key][:japanese]
+      library[key][:japanese] = value[1] 
+      #binding.pry 
+    end 
+end
 end
 
 def get_japanese_emoticon
